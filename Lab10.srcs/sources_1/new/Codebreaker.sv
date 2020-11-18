beeeb82b3a4e8ea1398d0da10901ce2c4346591d
Colby Weber <cweber@wiktel.com>
2020-11-18 12:57:55 -0700

    worked on codebreader module with ta

--- Lab10.srcs/sources_1/new/Codebreaker.sv
+++ Lab10.srcs/sources_1/new/Codebreaker.sv
@@ -27,54 +27,70 @@ module Codebreaker(
     input wire logic done_drawing_plaintext,
     output logic [127:0] plaintext_to_draw
     );
-    assign key_display = key;
-    assign plaintext_to_draw = {"Hello World!"};
-//    assign draw_plaintext = start;
-    typedef enum logic [2:0] {s0, s1, s2, s3, s4, ERR='X} StateType;
+    assign key_display = key[23:8];
+//    assign plaintext_to_draw = {"Hello World!"};
+    logic [127:0] correct;
+    assign correct = 128'h204543454e203232302049532046554e;
+    //assign plaintext_to_draw = correct;
+    typedef enum logic [2:0] {Idle, Decode, Transmit, Done, ERR='X} StateType;
     StateType ns, cs;
     
     logic [23:0] key;
     logic [127:0] cyphertext;
     assign key = 24'h79726a;
     assign cyphertext = 128'h93a931affae622e10a029bd3d4bd6ced;
-    logic dce, dcd;
+    logic dce, dcd; // decript enable, decript done
     decrypt_rc4 decript0(.clk(clk), .reset(reset), .enable(dce), .key(key),
-        .bytes_in(cyphertext), .bytes_out(/*plaintext_to_draw*/), .done(dcd));
+        .bytes_in(cyphertext), .bytes_out(plaintext_to_draw), .done(dcd));
     
     always_comb begin
-        dce = 0;
+        dce = 0; // decript enable
         draw_plaintext = 0;
         stopwatch_run = 0;
+        ns = ERR;
         if (reset)
-            ns = s0;
+            ns = Idle;
         else begin
             case(cs)
-                s0:
+                Idle: // wait for signal
                     if(start)
-                        ns = s1;
+                        ns = Decode;
                     else
-                        ns = s0;
-                s1: begin
-                    dce = 1;
+                        ns = Idle;
+                Decode: begin // wait for decode to be done
+                    dce = 1; // decode enable
                     stopwatch_run = 1;
-                    if (dcd)
-                        ns = s4;
+                    if (dcd) // decode done
+                        ns = Transmit;
                     else
-                        ns = s1;
+                        ns = Decode;
                 end
-                s2: begin
+                Transmit: begin // wait for transmit
+//                    dce = 1;
+//                    plaintext_to_draw = bitOut;
                     draw_plaintext = 1;
                     if (done_drawing_plaintext)
-                        ns = s3;
+                        ns = Done;
                     else
-                        ns = s2;
+                        ns = Transmit;
                 end
-                s3:
-                    ns = s3;
-                s4:
-                    ns = s2;
-                default:
-                    ns = ERR;
+                Done:
+                    ns = Done;
+//                s3:
+//                    if(start)
+//                        ns = s4;
+//                    else
+//                        ns = s3;
+//                s4: begin
+//                    plaintext_to_draw = 128'h204543454e203232302049532046554e;
+//                    draw_plaintext = 1;
+//                    if(done_drawing_plaintext)
+//                        ns = s0;
+//                    else
+//                        ns = s4;
+//                end
+//                default:
+///                    ns = ERR;
             endcase
         end
     end
